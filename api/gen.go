3de7ffe016ce6b152da387e2e3172ecf5846ee2d
Andrey Kontyakov <avkontya@mts.ru>
2022-03-02 12:20:16 +0300

    OAPI spec

---
+++ api/gen.go
@@ -0,0 +1,4 @@
+//nolint
+//go:generate oapi-codegen -package api -generate types,chi-server,spec -o api.gen.go api.swagger.yaml
+
+package api
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/andreykont/KVStorage
+
+go 1.16
